9ed8afe0154626066c376263b975ef302814913e
elCanarioVidal <pablo@umo.uy>
2017-11-28 15:31:07 -0300

    primer versión
    Carga manual de cervezas y estilos
    rotador de imagenes
    
    falta info de facebook y panel de carga de datos

--- js/canillas.js
+++ js/canillas.js
@@ -1,20 +1,40 @@
 var canillas = [
-		  'canilla 1',
-		  'canilla 2',
-		  'canilla 3',
-		  'canilla 4',
-		  'canilla 5',
-		  'canilla 6',
-		  'canilla 7',
-		  'canilla 8',
-		  'canilla 9',
-		  'canilla 10',
-		  'canilla 11',
-		  'canilla 12',
-		  'canilla 13',
-		  'canilla 14',
-		  'canilla 15',
-		  'canilla 16',
-		  'canilla 17',
-		  'canilla 18',
+		//Cambiar solo donde dice Variedad Birra y Marca Birra
+		'<span class="variedad celeste">Blonde</span><span class="marca amarillo"> Volcánica</span>',
+		//Cambiar solo donde dice Variedad Birra y Marca Birra
+		'<span class="variedad naranja">dubbel</span><span class="marca rojo"> Volcánica</span>',
+		//Cambiar solo donde dice Variedad Birra y Marca Birra
+		'<span class="variedad verde">ApA</span><span class="marca amarillo"> Beer Bros</span>',
+		//Cambiar solo donde dice Variedad Birra y Marca Birra
+		'<span class="variedad celeste">DARK STRONG ALE</span><span class="marca rojo"></span>',
+		//Cambiar solo donde dice Variedad Birra y Marca Birra
+		'<span class="variedad naranja">AMBER</span><span class="marca rojo"> Mist</span>',
+		//Cambiar solo donde dice Variedad Birra y Marca Birra
+		'<span class="variedad celeste">Gold</span><span class="marca amarillo"> Stolz</span>',
+		//Cambiar solo donde dice Variedad Birra y Marca Birra
+		'<span class="variedad rojo">Blonde</span><span class="marca verde"> Cabesas</span>',
+		//Cambiar solo donde dice Variedad Birra y Marca Birra	
+		'<span class="variedad amarillo">Stout</span><span class="marca celeste"> Cabesas</span>',
+		//Cambiar solo donde dice Variedad Birra y Marca Birra
+		'<span class="variedad verde">IPA</span><span class="marca naranja"> Cabesas</span>',
+		//fin parte izquierda
+		//parte derecha
+		//Cambiar solo donde dice Variedad Birra y Marca Birra
+		'<span class="variedad naranja">Scotish</span><span class="marca rojo"> Cabesas</span>',
+		//Cambiar solo donde dice Variedad Birra y Marca Birra
+		'<span class="variedad verde">IPA</span><span class="marca amarillo"> Volcánica</span>',
+		//Cambiar solo donde dice Variedad Birra y Marca Birra
+		'<span class="variedad celeste">APARevolution</span><span class="marca rojo"> Cabesas</span>',
+		//Cambiar solo donde dice Variedad Birra y Marca Birra
+		'<span class="variedad amarillo">ApA</span><span class="marca naranja"> BIMBA Bruder</span>',
+		//Cambiar solo donde dice Variedad Birra y Marca Birra
+		'<span class="variedad verde">Trigo</span><span class="marca amarillo"> Cabesas</span>',
+		//Cambiar solo donde dice Variedad Birra y Marca Birra
+		'<span class="variedad naranja">IPA</span><span class="marca celeste"> Oso Pardo</span>',
+		//Cambiar solo donde dice Variedad Birra y Marca Birra
+		'<span class="variedad rojo">Pilsener</span><span class="marca amarillo"> Martinez</span>',
+		//Cambiar solo donde dice Variedad Birra y Marca Birra
+		'<span class="variedad celeste">Golden</span><span class="marca verde"> 1983</span>',
+		//Cambiar solo donde dice Variedad Birra y Marca Birra
+		'<span class="variedad rojo">India Pale Ale</span><span class="marca verde"> Davok</span>',
 		];
